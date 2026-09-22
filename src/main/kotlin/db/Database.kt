b5f69fb3a462497f375bf76b270a7e2bcd3d8b94
lazar-jankovic <lj20210228@student.fon.bg.ac.rs>
2026-09-22 03:45:40 +0200

    izmena5

--- src/main/kotlin/db/Database.kt
+++ src/main/kotlin/db/Database.kt
@@ -9,7 +9,7 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 
 fun Application.configureDatabases() {
     // Izvlačimo iz okruženja (Render/Supabase) ili koristimo fallback za lokalni rad
-   /* val dbHost = System.getenv("DB_HOST") ?: "localhost"
+   val dbHost = System.getenv("DB_HOST") ?: "localhost"
     val dbPort = System.getenv("DB_PORT") ?: "5432"
     val dbName = System.getenv("DB_NAME") ?: "match_up"
     val user = System.getenv("DB_USER") ?: "postgres"
@@ -36,7 +36,8 @@ fun Application.configureDatabases() {
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         validate()
-    }*/
+    }
+    /*
 
 
     val url = environment.config.propertyOrNull("postgres.url")?.getString()
@@ -56,7 +57,7 @@ fun Application.configureDatabases() {
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         validate()
     }
-
+*/
     Database.connect(HikariDataSource(config))
 }
 
